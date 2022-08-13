d444e7c5ae33c609090ba50ab863363f5ef208ce
Feihong Hsu <feihong.hsu@gmail.com>
2022-08-13 15:55:28 -0500

    Draw some text

--- hello.js
+++ hello.js
@@ -1,12 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400)
+  textSize(52)
 }
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
+    fill(0)
   } else {
-    fill(255);
+    fill(255)
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, 80, 80)
+
+  fill(150, 200, 180)
+  text('你好世界！', 80, 200)
 }
