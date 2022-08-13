637b7c76fab76bc9dea1528da0cd2af6bb5335dd
Feihong Hsu <feihong.hsu@gmail.com>
2022-08-13 17:34:56 -0500

    Autobind methods of p5 instance object so they can be more easily used

--- i-5.js
+++ i-5.js
@@ -1,16 +1,28 @@
 export const name = 'Example I.5: Perlin noise walker'
 
+function autoBindMethods(s) {
+  const proto = Object.getPrototypeOf(s)
+  for (const name of Object.getOwnPropertyNames(proto)) {
+    const thing = proto[name]
+    if (typeof thing === 'function') {
+      s[name] = thing.bind(s)
+    }
+  }
+  return s
+}
+
 export default function (s) {
-  // let { text } = s
-  let text = s.text.bind(s)
+  autoBindMethods(s)
+
+  let { createCanvas, textSize, text } = s
 
   s.setup = () => {
-    s.createCanvas(400, 400)
-    s.textSize(52)
+    createCanvas(400, 400)
+    textSize(52)
   }
 
   s.draw = () => {
-    s.text('你好世界！', 80, 300)
+    text('你好世界！', 80, 300)
     text('你好世界！', 80, 200)
   }
 }
