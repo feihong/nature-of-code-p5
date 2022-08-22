b293b81ca76c450c4d6933321a9dba1921a746b5
Feihong Hsu <feihong.hsu@gmail.com>
2022-08-22 18:13:25 -0500

    Improve examples HTML generation

--- server.py
+++ server.py
@@ -2,50 +2,54 @@ import http.server
 import socketserver
 from pathlib import Path
 import io
+import re
 
 PORT = 8000
 
-def get_html_prefix(title):
+def get_examples_html():
+  def get_examples():
+    for example in Path('.').glob('Example *.js'):
+      m = re.match(r'Example (.*)  .*\.js', example.name)
+      yield f"""<li>
+        <a href="/examples/{m.group(1)}">
+          {example.stem}
+        </a>
+      </li>
+      """
+  examples = '\n'.join(get_examples())
+
   return f"""\
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>{title}</title>
+  <title>Examples</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+  <h1>Examples</h1>
+  <ul>
+  {examples}
+  </ul>
+</body>
+</html>
 """
-
-html_suffix = '</body></html>'
 
 class Handler(http.server.SimpleHTTPRequestHandler):
   def do_GET(self):
     if self.path == '/examples':
-      with self.list_examples() as f:
-        self.copyfile(f, self.wfile)
-      return
+      return self.list_examples()
 
     super().do_GET()
 
   def list_examples(self):
-    r = []
-    r.append(get_html_prefix('Examples'))
-    r.append('<h1>Examples</h1>\n<ul>\n')
-    for example in Path('.').glob('*.js'):
-      r.append(f'<li>{example}</li>\n')
-    r.append('</ul>')
-    r.append(html_suffix)
-    encoded = '\n'.join(r).encode('utf-8')
+    encoded = get_examples_html().encode('utf-8')
 
-    f = io.BytesIO()
-    f.write(encoded)
-    f.seek(0)
-    self.send_response(http.server.HTTPStatus.OK)
+    self.send_response(200)
     self.send_header("Content-type", "text/html; charset=utf-8")
     self.send_header("Content-Length", str(len(encoded)))
     self.end_headers()
-    return f
+    self.wfile.write(encoded)
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"serving at http://localhost:{PORT}")
